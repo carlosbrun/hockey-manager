372989dc622b6106fe408d6363cf0877a25d1a58
Carlos Brun <carlosbrun@gmail.com>
2024-11-17 20:27:45 +0100

    Test solución token, role y my_team no se propagan 9

--- backend/middlewares/connectMyTeamDb.js
+++ backend/middlewares/connectMyTeamDb.js
@@ -3,14 +3,14 @@ const Database = require('better-sqlite3');
 const mainDb = require('../database');
 
 function connectMyTeamDb(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
 
-    if (!token) {
-      return res.status(401).send("Token no proporcionado.");
-    }
+  if (!token) {
+    return res.status(401).send("Token no proporcionado.");
+  }
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
       return res.status(403).send("Token inválido.");
     }
@@ -21,23 +21,23 @@ function connectMyTeamDb(req, res, next) {
       return res.status(400).send("No se ha seleccionado un conjunto de equipos.");
     }
 
-  try {
-    // Busca el nombre de la base de datos en `main.db`
-    const query = `SELECT db_name FROM myteams WHERE myteam_id = ?`;
-    const row = mainDb.prepare(query).get(myteam_id); // Consulta síncrona para obtener `db_name`
+    try {
+      const query = `SELECT db_name FROM myteams WHERE myteam_id = ?`;
+      const row = mainDb.prepare(query).get(myteam_id);
 
-    if (!row) {
-      return res.status(500).send("Error al conectar con la base de datos del conjunto de equipos.");
+      if (!row) {
+        return res.status(500).send("Error al conectar con la base de datos del conjunto de equipos.");
+      }
+
+      const dbPath = path.join(__dirname, '../data', `${row.db_name}.db`);
+      req.teamDb = new Database(dbPath);
+      console.log(`Conectado a la base de datos: ${row.db_name}.db`);
+      next();
+    } catch (err) {
+      console.error("Error al conectar con la base de datos del conjunto de equipos:", err.message);
+      res.status(500).send("Error al conectar con la base de datos del conjunto de equipos.");
     }
-
-    const dbPath = path.join(__dirname, '../data', `${row.db_name}.db`);
-    req.teamDb = new Database(dbPath);
-    console.log(`Conectado a la base de datos: ${row.db_name}.db`);
-    next();
-  } catch (err) {
-    console.error("Error al conectar con la base de datos del conjunto de equipos:", err.message);
-    res.status(500).send("Error al conectar con la base de datos del conjunto de equipos.");
-  }
+  });
 }
 
 module.exports = connectMyTeamDb;
